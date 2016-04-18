a34e26888e0611fc299501062363a970bb0d91d8
wubalubadubdub <beargrillsllc@gmail.com>
2016-04-18 13:58:48 -0400

    added second activity

--- app/src/main/java/com/example/bearg/interactivestory/MainActivity.java
+++ app/src/main/java/com/example/bearg/interactivestory/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.bearg.interactivestory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameField;
+    private Button mStartButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameField = (EditText) findViewById(R.id.nameEditText);
+        mStartButton = (Button) findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                startStory(name);
+            }
+        });
+    }
+
+    private void startStory(String str) {
+        // keyword "this" is usually used to use the current Activity for a
+        // Context, since Activity is a subclass of Context and thus is a Context
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), str);
+        startActivity(intent);
     }
 }
